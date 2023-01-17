abe017c8d455427a724dafed8d57fb17cd1f0425
Nick Wright <nawright16@gmail.com>
2023-01-17 17:46:41 -0500

    all code for project

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,49 @@
-
 // Your Code Here
+//book, title, input
+//add event listener to save button to make request to update book
+// fetch request
+
+async function main() {
+ const response = await fetch('http://localhost:3001/listBooks')
+ const books = await response.json()
+ 
+ books.forEach(renderBook)
+}
+
+function renderBook(book) {
+    console.log(book)
+    const root = document.getElementById('root')
+
+    const li = document.createElement('li');
+    li.textContent = book.title
+
+    const input = document.createElement('input')
+    input.value = book.quantity
+
+    const saveButton = document.createElement('button')
+    saveButton.textContent = 'Save'
+
+    saveButton.addEventListener('click', () => {
+
+        const body = {
+            id: book.id,
+            quantity: input.value
+        }
+        fetch('http://localhost:3001/updatebook', {
+            method: 'PATCH',
+            headers: {
+                'content-type': 'application/json'
+            }, 
+            body: JSON.stringify(body)
+        })
+    })
+
+    li.append(input, saveButton)
+
+    root.appendChild(li)
+
+}
+main();
+
+
+
